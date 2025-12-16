5f09433be48c5436876e3e97cc12272c0b74db6b
sangwon <chosw0410@naver.com>
2025-12-16 18:33:59 +0900

    [feat] 벡엔드

---
+++ backend/src/main/java/com/sangwon/acamanager/AcaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sangwon.acamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcaManagerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sangwon/acamanager/AcaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangwon.acamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
